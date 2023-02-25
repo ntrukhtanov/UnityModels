9d6c1377707e1d4ea2c15e9783c44d6a753389fd
tnv <ntrukhtanov@gmail.com>
2023-02-25 16:40:07 +0300

    Пераработал обработку входящих параметров

--- Walker/MAPOCA/run_model.py
+++ Walker/MAPOCA/run_model.py
@@ -6,13 +6,27 @@ import torch
 from body_parts import WalkerBody
 from actor import ActorModel
 
-import sys
+import argparse
+import time
+from datetime import timedelta
 
 
 def run_model(walker_env_path, restore_path, env_worker_id, break_body_parts=None, break_body_parts_period=None):
     walker_body = WalkerBody()
-    body_model = dict()
 
+    # если нужно протестировать агентов со сломанными частями тела, то соберем соответствующие индексы
+    # в пространствах наблюдений и действий для последующего использования
+    obs_brake_idxs = list()
+    actions_breake_idxs = list()
+    if break_body_parts is not None:
+        broken_body_parts = break_body_parts.split(",")
+        for body_part in broken_body_parts:
+            body_part_prop = walker_body.body[body_part]
+            obs_brake_idxs.extend(
+                [idx for idx in body_part_prop.obs_space_idxs if idx not in walker_body.common_obs_space_idxs])
+            actions_breake_idxs.extend(body_part_prop.action_space_idxs)
+
+    body_model = dict()
     for body_part, body_part_property in walker_body.body.items():
         actor_model = ActorModel(body_part, body_part_property, walker_body.obs_size)
         actor_model.eval()
@@ -37,16 +51,28 @@ def run_model(walker_env_path, restore_path, env_worker_id, break_body_parts=Non
     # буферы для промежуточного хранения информации о статистике агентов
     agents_statistic = dict()
     step = 0
+    start_time = time.time()
+    time_2_break_body_parts = break_body_parts_period is None
     while True:
         ds, ts = env.get_steps(behavior_name)
         if ds.agent_id.shape[0] > 0:
             n_agents = ds.agent_id.shape[0]
             continious_actions = torch.zeros(n_agents, action_spec.continuous_size)
             input_data = torch.from_numpy(ds.obs[0])
+
+            # если есть сломанные части тела, то обнулим информацию о наблюдениях для этих частей тела
+            if len(obs_brake_idxs) > 0 and time_2_break_body_parts:
+                input_data[:, obs_brake_idxs] = 0.0
+
             for body_part, body_part_model in body_model.items():
                 actions, _ = body_part_model.forward(input_data)
                 idxs = walker_body.body[body_part].action_space_idxs
                 continious_actions[:, idxs] = actions
+
+            # если есть сломанные части тела, то обнулим информацию о действиях для этих частей тела
+            if len(actions_breake_idxs) > 0 and time_2_break_body_parts:
+                continious_actions[:, actions_breake_idxs] = 0.0
+
             action_tuple = ActionTuple()
             action_tuple.add_continuous(continious_actions.numpy())
 
@@ -80,47 +106,27 @@ def run_model(walker_env_path, restore_path, env_worker_id, break_body_parts=Non
         step += 1
         env.step()
 
+        if break_body_parts_period is not None and \
+                (time.time() - start_time) > timedelta(seconds=break_body_parts_period).seconds:
+            time_2_break_body_parts = ~time_2_break_body_parts
+            start_time = time.time()
+
 
 def run():
-    # TODO: прокомментировать и переписать красиво
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-walker_env_path')
+    parser.add_argument('-restore_path')
+    parser.add_argument('-env_worker_id', type=int)
+    parser.add_argument('-break_body_parts')
+    parser.add_argument('-break_body_parts_period', type=int)
 
-    args = sys.argv
+    args = parser.parse_args()
 
-    if '-walker_env_path' in args:
-        idx = args.index('-walker_env_path')
-        walker_env_path = args[idx + 1]
-    else:
-        walker_env_path = None
-
-    if '-restore_path' in args:
-        idx = args.index('-restore_path')
-        restore_path = args[idx + 1]
-    else:
-        restore_path = None
-
-    if '-env_worker_id' in args:
-        idx = args.index('-env_worker_id')
-        env_worker_id = int(args[idx + 1])
-    else:
-        env_worker_id = None
-
-    if '-break_body_parts' in args:
-        idx = args.index('-break_body_parts')
-        break_body_parts = args[idx + 1]
-    else:
-        break_body_parts = None
-
-    if '-break_body_parts_period' in args:
-        idx = args.index('-break_body_parts_period')
-        break_body_parts_period = int(args[idx + 1])
-    else:
-        break_body_parts_period = None
-
-    run_model(walker_env_path=walker_env_path,
-              restore_path=restore_path,
-              env_worker_id=env_worker_id,
-              break_body_parts=break_body_parts,
-              break_body_parts_period=break_body_parts_period)
+    run_model(walker_env_path=args.walker_env_path,
+              restore_path=args.restore_path,
+              env_worker_id=args.env_worker_id,
+              break_body_parts=args.break_body_parts,
+              break_body_parts_period=args.break_body_parts_period)
 
 
 if __name__ == '__main__':
